5ca89d8c8c2ba3e31f6933c79709995c4eb4f52f
Isormour <Kucharskifilip@o2.pl>
2024-02-24 16:04:26 +0100

    add shadows to lit non indirect shaders

--- RenderIndirect/AnimatorVATTestIndirect.cs
+++ RenderIndirect/AnimatorVATTestIndirect.cs
@@ -9,10 +9,12 @@ public class AnimatorVATTestIndirect : MonoBehaviour
     public Material mat;
     [SerializeField] float TimeSpeedMult = 0.1f;
     AnimatorVATIndirect vat;
+    float TimeStamp;
     public float VATTextureTime{ get { return vat.textureTime; } }
     public Transform CashedTransform { private set; get; }
     void Start()
     {
+        TimeStamp = Random.Range(0.0f, 1.0f);
         CashedTransform = this.transform;
         MaterialPropertyBlock matProperties = new MaterialPropertyBlock();
         vat = new AnimatorVATIndirect(matProperties,this.transform,mat,mesh,animatorController);
